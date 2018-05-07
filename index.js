1308962126c0b86c0b440a1e1f228088259ecec0
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-06 19:08:29 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity(`Munching and Crunching`);
+});
+
+// Event to listen to messages sent to the server where the bot is located
+bot.on('message', message => {
+    // So the bot doesn't reply to iteself
+    if (message.author.bot) return;
+  
+      if (message.content.indexOf("Udon" "udon") === 0) {
+    return message.channel.send("In my soba household?");
+    }
+  
+});
+
+bot.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = 'g!';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if(cmd ===`${prefix}botinfo`){
+
+    let bicon = bot.user.displayAvatarURL;
+    let botembed = new Discord.RichEmbed()
+    .setDescription("Bot Information")
+    .setColor("#00ff67")
+    .setThumbnail(bicon)
+    .addField("Bot Name", bot.user.username)
+
+    return message. channel.send(botembed);
+  }
+
+  if(cmd === `${prefix}sobaman`){
+    return message.channel.send("You got the wrong person, I'm not as handsome as him");
+  }
+});
+
+bot.login(process.env.token);
