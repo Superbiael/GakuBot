21407873fcf902cf499bab58b4dfa9626508a1b8
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-06 19:20:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity(`Munching and Crunching`);
+  bot.user.setActivity(`Soba?`);
 });
 
 // Event to listen to messages sent to the server where the bot is located
