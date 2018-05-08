e12473289f4a414ffa1160ee5e2e769a8c8a102f
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-08 11:25:55 -0700

    Update gakuhelp.js

--- commands/gakuhelp.js
+++ commands/gakuhelp.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot,message, args) => {
 
   let helpembed = new Discord.RichEmbed()
   .setColor("#a0a0a0")
-  .addField("y!commands:", "mafia (alias:maf) || sobaman || dab")
+  .addField("g!commands:", "mafia (alias:maf) || sobaman || dab")
   .addField("Other commands:", "udon || gakkyun")
   .addField("8ball command:","g!gaku <this isn't a command yet>");
 
