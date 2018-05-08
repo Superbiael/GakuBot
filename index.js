dd8dfffd2ea8a8b59affbbfd3929a017e8f37e70
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-08 11:21:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -23,65 +23,64 @@ fs.readdir("./commands/", (err, files) => {
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setPresence({game:{name:'Ashley Love Bot,type:0}});﻿
+  bot.user.setPresence({game:{name:'Satsujin love bot lol',type:0}});﻿
 });
 
 
-// Event to listen to messages sent to the server where the bot is located
 bot.on('message', message => {
-    // So the bot doesn't reply to iteself
-    if (message.author.bot) return;
-  
-    if (message.content.indexOf("Udon") === 0) {
+  if (message.author.bot) return;
+
+     msg = message.content.toLowerCase();
+    
+    if (msg.startsWith("udon") === 0) {
     return message.channel.send("In my soba household?");
     }
   
-    if (message.content.indexOf("Gakkyun") === 0) {
+    if (msg.startsWith("gakkyun") === 0) {
     return message.channel.send("Yuppi.");
     }
 
-    if (message.content.indexOf("Gaku, no") === 0) {
+    if (msg.startsWith("gaku, no") === 0) {
     return message.channel.send("Gaku, yes.");
     }
 
+  
 });
 
 bot.on("message", async message => {
-
-  if (message.author.bot) return;
-  if (message.channel.type === "dm") return;
+  if(message.author.box) return;
+  if(message.channel.type === "dm") return;
 
   let prefix = 'g!';
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
+  let messageArray = message.content.split( " ");
+  let cmd = messageArray[0]
   let args = messageArray.slice(1);
+
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
 
   if(cmd ===`${prefix}botinfo`){
 
-    let bicon = bot.user.displayAvatarURL;
-    let botembed = new Discord.RichEmbed()
+  let bicon = bot.user.displayAvatarURL;
+  let botembed = new Discord.RichEmbed()
     .setDescription("Bot Information")
-    .setColor("#a0a0a0")
+    .setColor("#00ff67")
     .setThumbnail(bicon)
     .addField("Bot Name", bot.user.username)
 
-    return message. channel.send(botembed);
+  return message. channel.send(botembed);
   }
 
-  if(cmd === `${prefix}sobaman`){
-    return message.channel.send("You got the wrong person, I'm not as handsome as him.");
-  }
-  
-  if(cmd === `${prefix}bathbomb`){
-    return message.channel.send("Finally, some good fucking food.");
+  if(cmd === `${prefix}dab`){
+    return message.channel.send("//dabs away my worries while sitting in a Mercedes-Benz®");
   }
   
   if(cmd === `${prefix}mafia` || cmd === `${prefix}maf`){
     return message.channel.send("maf maf");
   }
   
-  if(cmd === `${prefix}dab`){
-    return message.channel.send("//dabs away my worries while sitting in a Mercedes-Benz®");
+  if(cmd === `${prefix}sobaman`){
+    return message.channel.send("You got the wrong person, I'm not as handsome as him.");
   }
 });
 
