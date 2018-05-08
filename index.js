9e521dd86c022336322dc7ced278f5a95b9f611e
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-08 11:35:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ bot.on('message', message => {
     return message.channel.send("Gaku, yes.");
   }
 
-  if (msg.startsWith ("hey gays and gaku")) {
+  if (msg.startsWith ("hey gays)) {
     return message.channel.send("What about me?");
   }
 
