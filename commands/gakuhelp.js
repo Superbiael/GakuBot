8169db60be07151e79cddccb3454931b420020f7
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-09 10:27:17 -0700

    Update gakuhelp.js

--- commands/gakuhelp.js
+++ commands/gakuhelp.js
@@ -3,12 +3,12 @@ const Discord = require("discord.js");
 module.exports.run = async (bot,message, args) => {
 
   let helpembed = new Discord.RichEmbed()
-  .addDescription("Do not include < > when using commands.")
+  .setDescription("Do not include < > when using commands.")
   .setColor("#a0a0a0")
-  .addField("g!commands:", "mafia (alias:maf) || sobaman || dab")
-  .addField("Other commands:", "Udon || gakkyun || Good morning Gaku || Good night Gaku")
-  .addField("8ball command:","g!gaku <question> | I haven't added this command yet lol")
-  .addField("Send DM:", "g!send <@user> <message> | Currently disabled ");
+  .addField("y!commands:", "mafia (alias:maf) || leaf || ryo || order || car || c - ss rank")
+  .addField("Other commands:", "I can't believe Ryo is dead || I'm gonna munch || Yuppi || I love you Yuki || Good morning Yuki || Good night Yuki || Yuki if you're out there || Pika~")
+  .addField("8ball command:","y!yuki <question> | Disabled")
+  .addField("Send DM:", "g!send @user <message> | Disabled");
 
   message.channel.send(helpembed);
 }
