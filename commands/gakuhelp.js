67720ab54c5b46d03d1fa8f5fcb2c849a4a85d32
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-09 10:28:58 -0700

    Update gakuhelp.js

--- commands/gakuhelp.js
+++ commands/gakuhelp.js
@@ -5,8 +5,8 @@ module.exports.run = async (bot,message, args) => {
   let helpembed = new Discord.RichEmbed()
   .setDescription("Do not include < > when using commands.")
   .setColor("#a0a0a0")
-  .addField("y!commands:", "mafia (alias:maf) || leaf || ryo || order || car || c - ss rank")
-  .addField("Other commands:", "I can't believe Ryo is dead || I'm gonna munch || Yuppi || I love you Yuki || Good morning Yuki || Good night Yuki || Yuki if you're out there || Pika~")
+  .addField("g!commands:", "mafia (alias:maf) || sobaman || dab ")
+  .addField("Other commands:", "Udon || Good morning Gaku, Good night Gaku")
   .addField("8ball command:","y!yuki <question> | Disabled")
   .addField("Send DM:", "g!send @user <message> | Disabled");
 
