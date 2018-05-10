7e2689f9a15811c73c30cf7ba737ba1bf2d5a941
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-09 19:44:02 -0700

    Update gakuhelp.js

--- commands/gakuhelp.js
+++ commands/gakuhelp.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot,message, args) => {
   .setColor("#a0a0a0")
   .addField("g!commands:", "mafia (alias:maf) || sobaman || dab ")
   .addField("Other commands:", "Udon || Good morning Gaku || Good night Gaku || Hey gays")
-  .addField("8ball command:","g!gaku <question> | Disabled")
+  .addField("8ball command:","g!gaku <question>")
   .addField("Send DM:", "g!send @user <message> | Requires administrator permissions to delete command message after sending DM");
 
   message.channel.send(helpembed);
