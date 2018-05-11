246f7766138f9d0157fdc40d68b403ca4ccaaa53
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-11 14:23:54 -0700

    Update gakuball.js

--- commands/gakuball.js
+++ commands/gakuball.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
    if(!args[0]) return message.reply("Did you say something?");
-   let replies = ["Yes... Wait no. What I mean to say is... maybe I’ve said too much. ","That’s the way it should be (lol)","That won't be necessary.","Of course. Who do you think I am?", "What a sec, absolutely not!", "That's right, there's no way I could be wrong.","Hell no!", "Not in this soba household."];
+   let replies = ["Yes... no... looks like I said too much (lol).","That's the way it should be (lol)", "Of course. Who do you think I am?", "That won't be necessary.", "Hm... wait a second, you shouldn't be asking me about this!", "Wouldn't it be better if you decided on your own (lol).","Hell no!", "Huh... What's with that (lol)", "Huh? leave it to me.", "No, don't be ridiculous.", "That's right (lol)."];
 
    let result = Math.floor((Math.random() * replies.length));
    let question = args.slice(0).join(" ");
