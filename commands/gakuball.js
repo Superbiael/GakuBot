8e8399113c37f65ad90291bc07f4d87e1a6b7f7d
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-11 13:41:02 -0700

    Update gakuball.js

--- commands/gakuball.js
+++ commands/gakuball.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
    if(!args[0]) return message.reply("I'm gay, and can't read.");
-   let replies = ["Yes... Wait no. Maybe I’ve said too much. ","That’s the way it should be (lol)","That won't be necessary.","Of course. Who do you think I am?", "What a sec, absolutely not!", "That's right, there's no way I could be wrong.","Hell no!", "Not in this soba household."];
+   let replies = ["Yes... Wait no. What I mean to say is... maybe I’ve said too much. ","That’s the way it should be (lol)","That won't be necessary.","Of course. Who do you think I am?", "What a sec, absolutely not!", "That's right, there's no way I could be wrong.","Hell no!", "Not in this soba household."];
 
    let result = Math.floor((Math.random() * replies.length));
    let question = args.slice(0).join(" ");
