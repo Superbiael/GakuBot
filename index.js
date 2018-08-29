e1233440561a181e0a4c898ce28fb59a0d848c23
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-08-28 22:21:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ bot.on('message', message => {
       }
 
     if(msg.startsWith (prefix + "scout")) {
-        number = 60;
+        number = 61;
         var random = Math.floor (Math.random() * (number)) + 1;
         switch (random) {
           case 1: botschannel.send ({files:["./images/gaku_birthday_sr.png"]}); break;
@@ -114,6 +114,7 @@ bot.on('message', message => {
           case 58: botschannel.send ({files:["./images/gaku_heavenlynut_ur.png"]}); break;
           case 59: botschannel.send ({files:["./images/gaku_rti_ssr.png"]}); break;
           case 60: botschannel.send ({files:["./images/gaku_diafus_ssr.png"]}); break;
+          case 61: botschannel.send ({files:["./images/gaku+riku_shuffle_ssr.png"]}); break;
         }
       }
 
@@ -227,7 +228,7 @@ bot.on('message', message => {
     .setColor("#a0a0a0")
     .setThumbnail(bicon)
     .addField("# of quotes:", "15")
-    .addField("Scout:", "60 cards")
+    .addField("Scout:", "61 cards")
   return botschannel.send(botembed);
   }
 
