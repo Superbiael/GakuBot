9e47069452aafbe757e0b736a1af267a1378ee29
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-09-04 03:32:07 -0700

    Update gakuball.js

--- commands/gakuball.js
+++ commands/gakuball.js
@@ -2,7 +2,20 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
    if(!args[0]) return message.reply("Did you say something?");
-   let replies = ["Yes... no... looks like I said too much (lol).","That's the way it should be (lol)", "Of course. Who do you think I am?", "That won't be necessary.", "Hm... wait a second, you shouldn't be asking me about this!", "Wouldn't it be better if you decided on your own (lol).","Hell no!", "Huh... What's with that (lol)", "Huh? leave it to me.", "No, don't be ridiculous.", "That's right (lol)."];
+   let replies = [
+  "Yes... no... looks like I said too much (lol).",
+   "That's the way it should be (lol)",
+   "Of course. Who do you think I am?",
+   "That won't be necessary.",
+   "Hm... wait a second, you shouldn't be asking me about this!",
+   "Wouldn't it be better if you decided on your own (lol).",
+   "Hell no!",
+   "Huh... What's with that (lol)",
+   "Huh? leave it to me.",
+   "No, don't be ridiculous.",
+   "That's right (lol).",
+   "My answer is \nIt's a secret.",
+  ];
 
    let result = Math.floor((Math.random() * replies.length));
    let question = args.slice(0).join(" ");
@@ -14,7 +27,7 @@ module.exports.run = async (bot, message, args) => {
 
    let botschannel = message.guild.channels.find(`name`, "idolbot7");
    if(!botschannel) return message.channel.send("Couldn't find bot channel.");
-   botschannel.send(ballembed);
+   message.channel.send(ballembed);
 }
 
 module.exports.help = {
