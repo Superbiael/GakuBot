44fef34bffbc93bf701f71cb60ee9205b64a9e71
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-10-31 18:23:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -182,8 +182,6 @@ bot.on('message', message => {
       .setDescription("Bot Information")
       .setColor("#a0a0a0")
       .setThumbnail(bicon)
-      .addField("# of quotes:", "21")
-      .addField("Scout:", "66 cards")
     return botschannel.send(botembed);
   }
 
