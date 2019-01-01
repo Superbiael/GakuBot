adaa877c8addcb929854f2a5357087e6b2c6fcc0
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-12-31 17:47:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ bot.on("ready", async () => {
 });
 
 bot.on('message', message => {
-  if (message.author.bot) return;
+//   if (message.author.bot) return;
     if(message.channel.type === "dm") return;
     let prefix = 'g!';
     let messageArray = message.content.split(" ");
